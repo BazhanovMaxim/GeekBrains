bb25bf6af9a86daf95c4bb6cd3c0ee3ae0dfa58d
Maxim <bazhanov.maksim@list.ru>
2020-09-04 20:05:40 +0300

    Урок 5. Многопоточность. Часть II

---
+++ Multithreading_2/src/prev/Car.java
@@ -0,0 +1,9 @@
+package prev;
+
+import java.util.concurrent.Semaphore;
+
+public class Car extends Auto {
+    public Car(String id_auto, Semaphore semaphore) {
+        super(semaphore, id_auto, 20F, 2.5F);
+    }
+}
